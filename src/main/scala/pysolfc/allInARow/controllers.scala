0515c1d85e0a025c7b44a12f421d13bd4d0fd8aa
Kyle? <74930526+KyleWynnM@users.noreply.github.com>
2023-02-17 13:25:22 -0500

    added all in a row pysol variant

---
+++ src/main/scala/pysolfc/allInARow/controllers.scala
@@ -0,0 +1,23 @@
+package pysolfc.allInARow
+
+import org.combinators.cls.interpreter.ReflectedRepository
+import org.combinators.solitaire.domain.Solitaire
+import org.combinators.solitaire.shared.SolitaireDomain
+import org.combinators.solitaire.shared.python.PythonSemanticTypes
+import pysolfc.shared.GameTemplate
+
+trait controllers extends GameTemplate  with PythonSemanticTypes  {
+
+    // dynamic combinators added as needed
+    override def init[G <: SolitaireDomain](gamma : ReflectedRepository[G], s:Solitaire) :  ReflectedRepository[G] = {
+      var updated = super.init(gamma, s)
+      println(">>> All in a row Controller dynamic combinators.")
+
+      updated = updated
+        .addCombinator (new ProcessView(s))
+
+      updated
+    }
+
+
+}
